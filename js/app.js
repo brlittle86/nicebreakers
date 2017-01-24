c187628f51daf4967c07a5980da55b1cddd56da1
Brandon Little <brlittle86@gmail.com>
2017-01-24 10:14:56 -0800

    index made global index in local storage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
-var i = 0;
+//userIndex pulled from local storage if exists, else set index to 0
+var i = localStorage.getItem('globalIndex');
+if (i !== null) {
+  JSON.parse(i);
+} else {
+  var i = 0;
+}
 
 //userName pulled from local storage if exists, else write form for userName entry written by Brandon
 var storedUserName = localStorage.getItem('storedUserName');
