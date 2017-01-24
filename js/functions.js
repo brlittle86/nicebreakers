dd33f9021ecf543f306cb1eef85b8eab4ef48bab
Brandon Little <brlittle86@gmail.com>
2017-01-23 17:05:06 -0800

    added if/else statement and push username to local storage

--- js/functions.js
+++ js/functions.js
@@ -50,6 +50,8 @@ function renderScenario(i) {
 function nameSubmissionFunc(event) {
   event.preventDefault();
   event.stopPropagation();
+  storedUserName = event.target.username.value; // added username to local storage/global variable by Brandon
+  localStorage.storedUserName = JSON.stringify(storedUserName);
   removeNameSubmissionForm();
   renderScenario(i);
 }
