b6751dc178bed5ff6214231b364548c00b74e100
Stephanie <ssterli2@gmail.com>
2017-01-24 15:54:20 -0800

    debugging by Luay, Brandon, Stephanie, temporarily moved everything to functions file

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,47 @@
 'use strict';
+var answerZero;
+var answerOne;
+var answerTwo;
+var answerThree;
+var answerChosenZero = [];
+var answerChosenOne = [];
+var answerChosenTwo = [];
+var answerChosenThree = [];
+
+//userIndex pulled from local storage if exists, else set index to 0
+var i = localStorage.getItem('globalIndex');
+if (i !== null) {
+  JSON.parse(i);
+} else {
+  var i = 0;
+  console.log(i);
+}
+
+//userName pulled from local storage if exists, else write form for userName entry written by Brandon
+var storedUserName = localStorage.getItem('storedUserName');
+if (storedUserName !== null) {
+  JSON.parse(storedUserName);
+  // renderScenario(i);
+} else {
+  renderNameSubmissionForm();
+};
+
+//Stephanie created nameSubmissionEvent
+function nameSubmissionEvent(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  storedUserName = event.target.username.value; // added username to local storage/global variable by Brandon
+  localStorage.storedUserName = JSON.stringify(storedUserName);
+  removeNameSubmissionForm();
+  renderScenario(i);
+}
+
+//nameSubmission event listener written by Stephanie
+var nameSubmission = document.getElementById('form');
+if (nameSubmission !== null){
+  nameSubmission.addEventListener('submit', nameSubmissionEvent, false);
+}
+
 // Stephanie Typed render Name submission and remove it function
 function renderNameSubmissionForm(){
   var sectionEl = document.getElementById('content');
@@ -35,15 +78,55 @@ function renderScenario(i) {
   var scenarioEl = document.getElementById('scenario');
   scenarioEl.textContent = scenariosArray[i].scenario;
   var answerEl = document.getElementById('answers');
-  for (var j = 0; j < 4;j++) {
-    var renderP = document.getElementById('render-answer-' + j);
-    answerEl.textContent = scenariosArray[i].answerArray[j];
+  for (var j = 0; j < scenariosArray[i].answerArray.length; j++){
+    var labelEl = document.getElementById('label-' + j);
+    labelEl.textContent = scenariosArray[i].answerArray[j];
+    console.log(labelEl.textContent);
+    answerEl.appendChild(labelEl);
   }
-  var submitEl = getElementById('button');
-  submitEl.setAttribute('type', 'submit');
-  submitEl.textContent = 'Submit Answer';
-  scenarioEl.appendChild(submitEl);
+  scenarioEl.appendChild(answerEl);
 }
+
+var answerZero = document.getElementById('input-0');
+var answerOne = document.getElementById('input-1');
+var answerTwo = document.getElementById('input-2');
+var answerThree = document.getElementById('input-3');
+
+function changeZeroInput (event){
+  answerChosenZero.push(document.getElementById('input-0'));
+}
+
+function changeOneInput (event){
+  answerChosenOne.push(document.getElementById('input-1'));
+}
+
+function changeTwoInput (event){
+  answerChosenTwo.push(document.getElementById('input-2'));
+}
+
+function changeThreeInput (event){
+  answerChosenThree.push(document.getElementById('input-3'));
+}
+
+answerZero.addEventListener('change', changeZeroInput, false);
+
+answerOne.addEventListener('change', changeOneInput, false);
+
+answerTwo.addEventListener('change', changeTwoInput, false);
+
+answerThree.addEventListener('change', changeThreeInput, false);
+
+//Stephanie created event listener for scenarioSubmissionEvent
+function scenarioSubmissionEvent(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  console.log('scenario submission is running');
+  renderFeedback();
+}
+
+var scenarioSubmission = document.getElementById('scenario-form');
+scenarioSubmission.addEventListener('submit', scenarioSubmissionEvent, false);
+console.log(typeof(scenarioSubmission));
 
 //Luay created the Final Thank you note
 function thankYou() {
@@ -55,26 +138,46 @@ function thankYou() {
   thanks.appendChild(button);
   console.log('thank you note is running');
 }
+// LUAY re-write the scenario one we click on the Next button
+
+function nextButtonListener (event) {
+  event.preventDefault();
+  event.stopPropagation();
+  if (i === scenariosArray.length) {
+    thankYou();
+    localStorage.removeItem('globalIndex');
+  } else {
+    i++;
+    localStorage.globalIndex = JSON.stringify(i);
+    removeFeedback();
+    renderScenario(i);
+  }
+}
+
+var nextButton = document.getElementById('next-form');
+nextButton.addEventListener('click', nextButtonListener, false);
 
 //Stephanie created renderFeedback
 //NEEDS TO BE TESTED WITH ANSWERS
-function renderFeedback(i) {
-  if (event.target.id === 'render-answer-0'){
+function renderFeedback() {
+  console.log('render feedback is running');
+  if (answerChosenZero.length === 1){
     var renderP = document.getElementById('render-feedback-0');
-    answerEl.textContent = scenariosArray[i].answerArray[0];
-  } else if (event.target.id === 'render-answer-1'){
+    renderP.textContent = scenariosArray[i].feedbackArray[0];
+    answerChosenZero = [];
+  } else if (answerChosenOne.length === 1){
     var renderP = document.getElementById('render-feedback-1');
-    answerEl.textContent = scenariosArray[i].answerArray[1];
-  } else if (event.target.id === 'render-answer-2'){
+    renderP.textContent = scenariosArray[i].answerArray[1];
+    answerChosenOne = [];
+  } else if (answerChosenTwo.length === 1){
     var renderP = document.getElementById('render-feedback-2');
-    answerEl.textContent = scenariosArray[i].answerArray[2];
-  } else if (event.target.id === 'render-answer-3'){
+    renderP.textContent = scenariosArray[i].answerArray[2];
+    answerChosenTwo = [];
+  } else if (answerChosenThree.length === 1){
     var renderP = document.getElementById('render-feedback-3');
-    answerEl.textContent = scenariosArray[i].answerArray[3];
+    renderP.textContent = scenariosArray[i].answerArray[3];
+    answerChosenThree = [];
   }
-  var submitEl = document.getElementById('button');
-  submitEl.setAttribute('type', 'click');
-  submitEl.textContent = 'Next';
 }
 
 //Stephanie created removeFeedback
